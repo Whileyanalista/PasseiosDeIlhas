b81002031b2a81cf7e18b1811037defb59930a10
Whileyanalista <whiley.analista@gmail.com>
2026-06-26 10:28:44 -0300

    plugin-next.js

--- passeios-ilhas/next-runtime.js
+++ passeios-ilhas/next-runtime.js
@@ -1,4 +1,4 @@
-import { NetlifyNextPlugin } from "@netlify/plugin-nextjs";
+import { NetlifyNextPlugin } from "@netlify/plugin-next.js";
 
 export const onBuild = async ({ constants }) => {
   console.log("Netlify Forms migration applied");
